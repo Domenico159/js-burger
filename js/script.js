0c94f7cbe0f5d891ba69b93a0d3074a63db16c35
Domenico159 <domenico.zummo@gmail.com>
2021-03-26 16:17:18 +0100

    Add btn price total

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 var btn = document.getElementById('button') ;
 var nameUser = document.getElementById('name');
 var ingredients = document.getElementsByClassName('ingredient-checkbox');
+var priceTotal = document.getElementById('price');
 
 
 
@@ -23,5 +24,21 @@ btn.addEventListener('click',
                 }else{
                     // console.log('ok name is ' + nameBurger);
                     var price = 50;
-                }
+
+                    for(var i = 0 ; i < ingredients.length; i++){
+                        var ingredientCheck = ingredients[i];
+
+                        if(ingredientCheck.checked){
+
+                         price += parseInt(ingredientCheck.value);
+
+                        //  console.log(price);
+
+                        //  console.log(ingredientCheck.value)
+                        
+                        }
+                    }
+
+                    priceTotal.innerHTML = price.toFixed(2);
+                }                         
             })
